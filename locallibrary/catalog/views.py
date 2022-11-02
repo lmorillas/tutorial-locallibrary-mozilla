82c7c3158bf48fe04c669571436787e7b7ebcecc
lmorillas <morillas@gmail.com>
2022-11-02 10:12:51 +0100

    libros

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Book
 
 # Create your views here.
 def index(request):
     texto = '''<h1>Librería Local</h1>
     <p>Esta es la página principal de la librería local.</p>'''
     # texto = 'Página inicial de la librería local'
-    return HttpResponse(texto)
+    lista = '<h2>Mi lista de últimos libros</h2><ul>'
+    # Consulta a la base de datos: últimos 5 libros
+    for libro in Book.objects.all()[:5]:
+        lista += f'<li>{libro.title}</li>'
+    lista += '</ul>'
+
+    return HttpResponse(texto + lista)
 
 def acerca_de(request):
     texto = '''<h1>Acerca de</h1>

3449b9e9c14ed34946aba15770b452ed35702436
lmorillas <morillas@gmail.com>
2022-11-07 10:03:59 +0100

    clase

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 # https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Models
 # Create your models here.
@@ -29,7 +30,7 @@ class Book(models.Model):
 
 class Genre(models.Model):
     """Model representing a book genre."""
-    name = models.CharField(max_length=200, 
+    name = models.CharField(max_length=180, 
         help_text='Enter a book genre (e.g. Science Fiction)')
 
     def __str__(self):
@@ -58,3 +59,35 @@ class Language(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.name
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, 
+        default=uuid.uuid4, 
+        help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey('Book', 
+        on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200, blank=True)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id} - {self.book.title}'
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from catalog.models import Book
+from catalog.models import Book, BookInstance, Author
+
 
 # Create your views here.
-def index(request):
+def index_general(request):
     texto = '''<h1>Librería Local</h1>
     <p>Esta es la página principal de la librería local.</p>'''
     # texto = 'Página inicial de la librería local'
-    lista = '<h2>Mi lista de últimos libros</h2><ul>'
+    lista = '<h2>Mi from django.shortcuts import renderlista de últimos libros</h2><ul>'
     # Consulta a la base de datos: últimos 5 libros
     # for libro in Book.objects.all()[:5]:
     # 5 últimos
@@ -25,6 +26,34 @@ def acerca_de(request):
     '''
     return HttpResponse(texto)
 
+def index(request):
+    """View function for home page of site."""
 
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
 
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+    ultimos = Book.objects.all().order_by('-id')[:10]
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books':num_books,
+            'num_instances':num_instances,
+            'num_instances_available':num_instances_available,
+            'num_authors':num_authors,
+            'num_visits':num_visits,
+            'ultimos':ultimos},
+    )
 
